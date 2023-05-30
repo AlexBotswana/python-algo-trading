67b502b75fe1a61c895502da60502f8347f63fc5
AlexBotswana <alfacama@outlook.fr>
2023-05-30 22:19:20 +0200

    knapsack

--- trading/optimized.py
+++ trading/optimized.py
@@ -1,45 +1,92 @@
-import  csv
-import  time
+from tqdm import tqdm
+import csv
+import time
+import sys
 
-def optimized(max_invest, data):
-    nb_stocks = len(data)
-    best_invest = 0
-    best_performance = 0
-    lst_best_invest = []
-    
-    def combinations(lst_stocks, idx, invest, performance):
-        nonlocal best_invest, best_performance, lst_best_invest
-        
-        if idx == nb_stocks:
-            if performance > best_performance:
-                best_invest = invest
-                best_performance = round(performance, 2)
-                lst_best_invest = lst_stocks
-            return
-        
-        stock = data[idx]
-        if invest + float(stock[1]) <= max_invest:
-            combinations(lst_stocks + [stock], idx + 1, invest + float(stock[1]), performance + float(stock[1])*float(stock[2]))
-        combinations(lst_stocks, idx + 1, invest, performance)
-    
-    combinations([], 0, 0, 0)
-    
-    print(f'\nInvest amount: {best_invest}€\nperformance: {best_performance}€\nStocks list: {lst_best_invest}\n')
+start_time = time.time()
+# Check for custom cash investment (default = 500)
+try:
+    MAX_INVEST = float(sys.argv[2])
+except IndexError:
+    MAX_INVEST = 500
 
-lst_stocks = []
 
-# Open the CSV file and insert data into the table
-with open('db/dataset1.csv') as csvfile:
-# with open('db/data_stocks2.csv') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=";")
-    #to remove first line (file's header)
-    next(csvreader)
-    for row in csvreader:
-        lst_stocks.append(row)
-    
-begin = time.time()
-optimized(500, lst_stocks)
-end = time.time()
+def main():
+    # Check for filename input
+    try:
+        filename = "db/" + sys.argv[1] + ".csv"
+    except IndexError:
+        print("\nNo filename found. Please try again.\n")
+        time.sleep(1)
+        sys.exit()
+    stocks_list = read_csv(filename)
+    print(f"\nProcessing '{sys.argv[1]}' ({len(stocks_list)} valid stocks) for {MAX_INVEST}€ :")
+    display_results(knapsack(stocks_list))
 
-time_exec = end - begin
-print("Execution time: ", time_exec, "seconds")
+def read_csv(filename):
+    # Import stocks data from csv file
+    # Filter out corrupted data
+    try:
+        with open(filename) as csvfile:
+            stocks_file = csv.reader(csvfile, delimiter=';')
+            next(csvfile)       # skip first row
+            stocks_list = []
+            for row in stocks_file:
+                if float(row[1]) <= 0 or float(row[2]) <= 0:
+                    pass
+                else:
+                    stock = (
+                        row[0],
+                        int(float(row[1])*100),
+                        float(float(row[1]) * float(row[2])/100)
+                    )
+                    stocks_list.append(stock)
+            return stocks_list
+    except FileNotFoundError:
+        print(f"\nFile '{filename}' does not exist. Please try again.\n")
+        time.sleep(1)
+        sys.exit()
+
+def knapsack(stocks_list):
+    # Initialize the matrix (ks) for 0-1 knapsack problem
+    # Get best stocks combination
+    max_inv = int(MAX_INVEST * 100)     
+    stocks_total = len(stocks_list)
+    cost = []       
+    profit = []     
+    for stock in stocks_list:
+        cost.append(stock[1])
+        profit.append(stock[2])
+    # Find optimal profit
+    ks = [[0 for x in range(max_inv + 1)] for x in range(stocks_total + 1)]
+    for i in tqdm(range(1, stocks_total + 1)):
+        for w in range(1, max_inv + 1):
+            if cost[i-1] <= w:
+                ks[i][w] = max(profit[i-1] + ks[i-1][w-cost[i-1]], ks[i-1][w])
+            else:
+                ks[i][w] = ks[i-1][w]
+    # Retrieve combination of stocks from optimal profit
+    best_combo = []
+    while max_inv >= 0 and stocks_total >= 0:
+        if ks[stocks_total][max_inv] == \
+                ks[stocks_total-1][max_inv - cost[stocks_total-1]] + profit[stocks_total-1]:
+            best_combo.append(stocks_list[stocks_total-1])
+            max_inv -= cost[stocks_total-1]
+        stocks_total -= 1
+    return best_combo
+
+def display_results(best_combo):
+    # Display best combination results
+    print(f"\nMost profitable investment ({len(best_combo)} stocks) :\n")
+    cost = []
+    profit = []
+    for item in best_combo:
+        print(f"{item[0]} | {item[1] / 100} € | +{item[2]} €")
+        cost.append(item[1] / 100)
+        profit.append(item[2])
+    print("\nTotal cost : ", sum(cost), "€")
+    print("Profit after 2 years : +", sum(profit), "€")
+    print("\nTime elapsed : ", time.time() - start_time, "seconds\n")
+
+if __name__ == "__main__":
+    main()
